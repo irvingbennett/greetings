fa7643ec6eaa52e4a6413e0a78117214ebf8b60d
Irving Bennett <irving@alairelibre.net>
2021-03-17 17:54:13 -0500

    Added error message

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 // Package greetings returns a sting
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a for the named person
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name gas given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// Return greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
